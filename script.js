f41855de036875b40cb11d92ec2e70f1ce0436a7
Godspower Omokpia <omokpia59@gmail.com>
2025-08-15 13:58:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-let introduction = "My name is Godspower. I am a Mobile developer"
+// let introduction = "My name is Godspower. I am a Mobile developer"
 
-let age = 25;
+// let age = 25;
 
-let gender = "Male";
+// let gender = "Male";
 
-let hobbies = ["coding", "reading", "gaming"];
+// let hobbies = ["coding", "reading", "gaming"];
 
-let skills = {
-    programmingLanguages: ["JavaScript", "Python", "Java"],
-    frameworks: ["React", "Node.js", "Django"]
-}
+// let skills = {
+//     programmingLanguages: ["JavaScript", "Python", "Java"],
+//     frameworks: ["React", "Node.js", "Django"]
+// }
 
-let education = {
-    degree: "Bachelor of Science in Computer Science",
-    institution: "University of Technology",
-    graduationYear: 2020
-}
+// let education = {
+//     degree: "Bachelor of Science in Computer Science",
+//     institution: "University of Technology",
+//     graduationYear: 2020
+// }
 
-let maritalStatus = "Single";
+// let maritalStatus = "Single";
+
