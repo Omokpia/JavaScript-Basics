fdeecdbc3cc3925840e550c1a1b0ba4dcc60db55
Godspower Omokpia <omokpia59@gmail.com>
2025-08-17 23:54:45 +0100

    new name

--- variables.js
+++ variables.js
@@ -28,16 +28,15 @@
 
 // how to create a variable
 // var , let , const
+// const GENDER = "MALE";
+// const PI = 3.14;
 
-const GENDER = "MALE";
-const PI = 3.14;
+// let firstName = "Godspower";
+// let lastName = "Omokpia";
+// let age = 25;
+// let occupation = "Mobile Developer";
 
-let firstName = "Godspower";
-let lastName = "Omokpia";
-let age = 25;
-let occupation = "Mobile Developer";
-
-console.log(lastName);
+// console.log(lastName);
 
 
 // DATA TYPES//
@@ -45,6 +44,19 @@ console.log(lastName);
 // reference data types
 
 //PRIMITIVE DATA TYPES
-// string, number, boolean, null, undefined, symbol
+ // string, number, boolean, null, undefined, symbol
 
+
+// let firstName = 'Chioma'
+// let surname = 'Rowland'
+// let fullName = 'Rowland Chioma'
+// let age = 24
+// let maritalStatus = 'Single'
+
+// She got married and it's a new year
+
+// surname = 'Adeleke'
+// age = 25
+
+// console.log('This is an information concerning a lady called Chioma,', firstName, surname, 'and she is', age, 'years old.')
 
