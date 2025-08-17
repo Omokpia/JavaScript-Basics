3f21b807283479550bc9099a13a2de7c3d346fa3
Godspower Omokpia <omokpia59@gmail.com>
2025-08-18 00:20:25 +0100

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -11,5 +11,26 @@
 // 3) BOOLEAN
 // 4) NULL
 // 5) UNDEFINED
+// 6) SYMBOL (ES6)
 
+// 1) STRING: 
 
+// Anything inside of a quote either single quote ('') or double quote ("") is a string.
+
+// Example:
+
+let firstName = "Godspower";
+let lastName = "Omokpia";
+let favColor = 'white'
+let bills = '1000000'
+
+// 2) NUMBER:
+
+// Numbers can be integers either positive or negative or floating point numbers.
+
+// Example:
+let age = 25;
+let height = 5.9;
+let temperature = -10;
+let cgpa = 4.59
+let price = 1500;
