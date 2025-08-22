7d429f6ae89731fd4196e3d397e81511181f09c9
Godspower Omokpia <omokpia59@gmail.com>
2025-08-22 20:10:28 +0100

    few codes about conditionals

--- conditionals.js
+++ conditionals.js
@@ -59,3 +59,48 @@
 // }
 
 
+// TRUTHY AND FALSY VALUES IN JAVASCRIPT //
+
+// truthy values means, anything that is evaluated to a boolean and it is true, is a truthy value
+// falsy values mean, anything that is evaluated to a boolean and it is false, is a falsy value.
+// truthy values are values that evaluate to true in a boolean context
+// falsy values are values that evaluate to false in a boolean context
+
+// Truthy values examples:
+
+// 1. true
+// 2. any non-zero number (positive or negative)    
+// 3. any non-empty string
+// 4. any object (including arrays and functions)
+// 5. any non-null value
+// 6. any non-undefined value
+// 7. Symbols
+// 8. BigInt values (except 0n)
+// 9. Infinity and -Infinity
+// 10. Dates
+
+// TRUTHY VALUES: true, non-empty strings, numbers, objects, arrays, functions, arrays.
+
+// FALSY VALUES: false, empty strings,0, null, undefined, NaN
+
+// if(true){
+
+//     console.log('This is a truthy value');
+// }
+
+// console.log('5' + '5')
+
+// console.log('5' - 4)
+
+if(undefined){
+    console.log('The if block is evaluated')
+}
+
+else if('omokpia'){
+    console.log('The else if block is evaluated')
+}
+
+else{
+    console.log('The else block is evaluated')
+}
+
--- functions.js
+++ functions.js
@@ -1,3 +1,15 @@
-// array methods    
+// FUNCTIONS //
+
+// A function is a piece of code that performs a specific action, and it can be reused multiple times.
+// A function is a piece of reusable code that can be called with a specific name.
+
+
+
+
+
+
+
+
+
 
 
