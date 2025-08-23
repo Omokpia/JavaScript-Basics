46b1b7bfc7980ae1b642e51e90892b13df18c2cf
Godspower Omokpia <omokpia59@gmail.com>
2025-08-23 19:54:18 +0100

    Update loops.js

---
+++ loops.js
@@ -0,0 +1,19 @@
+// // loops//
+
+// // for(let i = 0; i < 10; i ++){
+// //     console.log(i)
+// // }
+
+// for(let i = 1; i <= 10; i ++){
+//     console.log(i)
+// }
+
+
+// for(let i = 1; i <= 10; i ++){
+    
+// }
+
+
+
+
+console.log('5' - 3)
